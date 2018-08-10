c6b758cf6ebd5963978db25b095525abaf646b90
RickGelhausen <rick.gelhausen@gmail.com>
2018-08-10 12:26:27 +0200

    Fixing theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.bibtex',
-    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,7 +76,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+RTD_NEW_THEME = True
+html_theme = ""
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
